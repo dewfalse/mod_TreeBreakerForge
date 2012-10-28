8a7e84d2a445a6f98f2b82423471ae848759a644
dewfalse <dewfalse@gmail.com>
2012-10-28 23:10:42 +0900

    不要なコメントを削除

--- mod_TreeBreaker/CommonProxy.java
+++ mod_TreeBreaker/CommonProxy.java
@@ -9,25 +9,23 @@ public class CommonProxy implements IGuiHandler {
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world,
 			int x, int y, int z) {
-		// TODO 自動生成されたメソッド・スタブ
 		return null;
 	}
 
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world,
 			int x, int y, int z) {
-		// TODO 自動生成されたメソッド・スタブ
 		return null;
 	}
 
 	public void registerTickHandler() {
-		// TODO 自動生成されたメソッド・スタブ
 
 	}
 
 	public void addKeyBinding() {
-		// TODO 自動生成されたメソッド・スタブ
+	}
 
+	public void registerCommand() {
 	}
 
 }
